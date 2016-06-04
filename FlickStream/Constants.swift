59f63213f24232ae17cd2a9b779f35ab660fd190
Arunoday Sarkar <a.sarkar.arun@gmail.com>
2016-06-04 11:04:22 -0400

    Added a constants file

---
+++ FlickStream/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  FlickStream
+//
+//  Created by Arunoday Sarkar on 6/4/16.
+//  Copyright © 2016 Sark Software LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+	
+	struct FlickrAPIKeys {
+		static let API_KEY = ""
+		static let API_SECRET = ""
+	}
+	
+}
